e9102018b025ec27d876c13fa1e6dcbc082f76f8
knknkn1162 <knknkn1162@gmail.com>
2018-01-01 00:07:24 +0900

    impl login-> show

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,12 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def loggin_in?
+  def logged_in?
     !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
