be3067d06aac9f2b8c89cd37c9e0b664809e738c
knknkn1162 <knknkn1162@gmail.com>
2018-01-01 14:37:17 +0900

    impl index view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
   test "should get new" do
     get signup_path
@@ -11,8 +12,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect edit when not logged in' do
-    get edit_user_path(@user)
-    assert_not flash.empty?
+    get users_path(@user)
     assert_redirected_to login_url
   end
 
