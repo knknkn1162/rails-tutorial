f9a8c5bed1183fe08c3ffb1ac8d573e19228e5b7
knknkn1162 <knknkn1162@gmail.com>
2018-01-01 14:29:40 +0900

    forward properly for others not to access your own page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -38,5 +41,21 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # defore action
+  def logged_in_user
+    if !logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      redirect_to(root_url)
+    end
   end
 end
