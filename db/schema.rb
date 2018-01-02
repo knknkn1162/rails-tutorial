bc6910d44791c8579f0658086e031459f45712dc
knknkn1162 <knknkn1162@gmail.com>
2018-01-02 13:07:26 +0900

    impl micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102032727) do
+ActiveRecord::Schema.define(version: 20180102035240) do
 
   create_table "microposts", force: :cascade do |t|
-    t.text "contect"
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
